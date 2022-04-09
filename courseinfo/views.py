e59e20754d712ed53eac22655414fcff9c237643
Xiuyuan Wang <xiuyuan7@illinois.edu>
2022-04-09 16:11:24 -0500

    complete CreateViews refactor

--- courseinfo/views.py
+++ courseinfo/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from courseinfo.models import Instructor, Section, Course, Semester, Student, Registration
 from courseinfo.forms import InstructorForm, SectionForm, CourseForm, SemesterForm, StudentForm, RegistrationForm
-from courseinfo.utils import ObjectCreateMixin, PageLinksMixin
+from courseinfo.utils import PageLinksMixin
 
 
 class InstructorList(PageLinksMixin, ListView):
@@ -22,9 +22,9 @@ class InstructorDetail(DetailView):
         return context
 
 
-class InstructorCreate(ObjectCreateMixin, View):
+class InstructorCreate(CreateView):
     form_class = InstructorForm
-    template_name = 'courseinfo/instructor_form.html'
+    model = Instructor
 
 
 class InstructorUpdate(View):
@@ -91,9 +91,9 @@ class SectionDetail(DetailView):
         return context
 
 
-class SectionCreate(ObjectCreateMixin, View):
+class SectionCreate(CreateView):
     form_class = SectionForm
-    template_name = 'courseinfo/section_form.html'
+    model = Section
 
 
 class SectionUpdate(View):
@@ -154,9 +154,9 @@ class CourseDetail(DetailView):
         return context
 
 
-class CourseCreate(ObjectCreateMixin, View):
+class CourseCreate(CreateView):
     form_class = CourseForm
-    template_name = 'courseinfo/course_form.html'
+    model = Course
 
 
 class CourseUpdate(View):
@@ -217,9 +217,9 @@ class SemesterDetail(DetailView):
         return context
 
 
-class SemesterCreate(ObjectCreateMixin, View):
+class SemesterCreate(CreateView):
     form_class = SemesterForm
-    template_name = 'courseinfo/semester_form.html'
+    model = Semester
 
 
 class SemesterUpdate(View):
@@ -281,9 +281,9 @@ class StudentDetail(DetailView):
         return context
 
 
-class StudentCreate(ObjectCreateMixin, View):
+class StudentCreate(CreateView):
     form_class = StudentForm
-    template_name = 'courseinfo/student_form.html'
+    model = Student
 
 
 class StudentUpdate(View):
@@ -341,9 +341,9 @@ class RegistrationDetail(DetailView):
         return context
 
 
-class RegistrationCreate(ObjectCreateMixin, View):
+class RegistrationCreate(CreateView):
     form_class = RegistrationForm
-    template_name = 'courseinfo/registration_form.html'
+    model = Registration
 
 
 class RegistrationUpdate(View):
