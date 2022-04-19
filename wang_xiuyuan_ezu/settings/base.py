e1a1dccc0d9744f788c5977dd6457727dc6b8890
Xiuyuan Wang <xiuyuan7@illinois.edu>
2022-04-18 20:23:29 -0500

    done development and production settings

--- wang_xiuyuan_ezu/settings/base.py
+++ wang_xiuyuan_ezu/settings/base.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'wang_xiuyuan_ezu.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '../db.sqlite3',
     }
 }
 
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
 LOGIN_REDIRECT_URL = 'about_urlpattern'
 
